d8140eb27ca549c1d1d0471056d92fd202119e7e
palloncino <antonio.guiotto.dev@gmail.com>
2024-10-26 13:57:23 +0200

    progress

--- wordpress-site/wp-content/themes/template-parts/header-custom.php
+++ wordpress-site/wp-content/themes/template-parts/header-custom.php
@@ -5,11 +5,11 @@
     </div>
     <div class="header-navigation-links-container">
         <div class="header-navigation-links">
-            <a class="header-navigation-link selected" href="/a-proposito-di-noi.html">A proposito di noi</a>
-            <a class="header-navigation-link" href="/cosa-facciamo.html">Cosa facciamo</a>
-            <a class="header-navigation-link" href="/over-the-edge.html">Over the Edge</a>
-            <a class="header-navigation-link" href="/il-gruppo.html">Il gruppo</a>
-            <a class="header-navigation-link" href="/contatti.html">Contatti</a>
+            <a class="header-navigation-link selected" href="/a-proposito-di-noi">A proposito di noi</a>
+            <a class="header-navigation-link" href="/cosa-facciamo">Cosa facciamo</a>
+            <a class="header-navigation-link" href="/over-the-edge">Over the Edge</a>
+            <a class="header-navigation-link" href="/il-gruppo">Il gruppo</a>
+            <a class="header-navigation-link" href="/contatti">Contatti</a>
             <a class="header-navigation-link" href="#">it <span style="font-family: arial;">|</span> en</a>
         </div>
     </div>
